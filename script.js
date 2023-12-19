8fe9a2afa250424676043f1e93bceccb2c753b24
LeilanNaeimi <leilan.n@gmail.com>
2023-12-19 18:43:22 +0300

    fixed add to library

--- script.js
+++ script.js
@@ -1,60 +1,49 @@
 let openDialogBox = document.getElementById("openDialogBox");
-
-let myLibrary = [];
-// const myLibrary = [
-//   {
-//     title: "Book 1",
-//     author: "Authour 1",
-//     pages: 100,
-//     read: "Y",
-//     info: function () {
-//       return `${this.title} by ${this.author},${this.pages} pages,
-//       ${
-//         this.read === "Y" ? `I read it before` : `Dont read yet`
-//       } and serial no is: 10000}`;
-//     },
-//   },
-//   {
-//     title: "Book 2",
-//     author: "Authour 2",
-//     pages: 512,
-//     read: "N",
-//     info: function () {
-//       return `${this.title} by ${this.author},${this.pages} pages,
-//         ${
-//           this.read === "Y" ? `I read it before` : `Dont read yet`
-//         } and serial no is: 10000}`;
-//     },
-//   },
-// ];
-
 let card = document.querySelector(".card");
 let ul = document.querySelector("ul");
 let item = "";
 
-function showBooks(myLibrary) {
-  console.log(myLibrary.length);
+const myLibrary = [
+  {
+    title: "Book 1",
+    author: "Author 1",
+    pages: 100,
+    read: "Y",
+    info: function () {
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${
+        this.read === "Y" ? `I read it before` : `I haven't read it yet`
+      }`;
+    },
+  },
+  {
+    title: "Book 2",
+    author: "Author 2",
+    pages: 512,
+    read: "N",
+    info: function () {
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${
+        this.read === "Y" ? `I read it before` : `I haven't read it yet`
+      }`;
+    },
+  },
+];
+
+function showBooks() {
+  ul.innerHTML = "";
   for (let i = 0; i < myLibrary.length; i++) {
-    console.log(myLibrary[i].title);
     let li = document.createElement("li");
-    item = document.createTextNode(myLibrary[i].title);
-    // `Title: ${myLibrary[i].title},Author  ${myLibrary[i].author}, Pages ${myLibrary[i].pages}.`
+    item = document.createTextNode(myLibrary[i].info());
     li.appendChild(item);
     ul.appendChild(li);
   }
 }
 
-// showBooks(myLibrary);
-
-/******** add book btn */
+showBooks();
 
 // Add a click event listener to the "Open Dialog Box" button
 openDialogBox.addEventListener("click", function () {
   let userDialog = document.getElementById("userDialog");
-  let outputBox = document.querySelector(".output");
   let confirmBtn = document.getElementById("confirmBtn");
-  let addToLibraryBtn = document.createElement("button");
-  let addToLibrary = document.createTextNode("Add to library");
 
   // Define the modalOpen function
   function modalOpen() {
@@ -67,26 +56,28 @@ openDialogBox.addEventListener("click", function () {
     let authorValue = document.getElementById("author").value;
     let pagesValue = document.getElementById("pages").value;
 
-    outputBox.textContent = `Book Title: ${bookTitleValue}, Author: ${authorValue}, Pages: ${pagesValue}   `;
+    // Create a new book object
+    const newBook = {
+      title: bookTitleValue,
+      author: authorValue,
+      pages: pagesValue,
+      read: "N",
+      info: function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${
+          this.read === "Y" ? `I read it before` : `I haven't read it yet`
+        }`;
+      },
+    };
 
-    myLibrary.title = bookTitleValue;
-    myLibrary.author = authorValue;
-    myLibrary.pages = pagesValue;
+    // Add the new book to the library
+    myLibrary.push(newBook);
 
-    // Close the dialog after displaying the values
+    // Update the display
+    showBooks();
     closeDialog();
-    addToLibraryBtn.appendChild(addToLibrary);
-    outputBox.appendChild(addToLibraryBtn);
   });
 
-  addToLibraryBtn.addEventListener("click", addBookToLibrary);
-
-  function addBookToLibrary() {
-    showBooks(myLibrary);
-    console.log("add to library");
-  }
-
-  //  close the dialog
+  // Close the dialog
   function closeDialog() {
     userDialog.close();
   }
@@ -94,3 +85,85 @@ openDialogBox.addEventListener("click", function () {
   // Show the modal dialog
   modalOpen();
 });
+
+//*********************** */
+//****************** */
+// let openDialogBox = document.getElementById("openDialogBox");
+// let card = document.querySelector(".card");
+// let ul = document.querySelector("ul");
+// let item = "";
+
+// const myLibrary = [
+//   {
+//     title: "Book 1",
+//     author: "Authour 1",
+//     pages: 100,
+//     read: "Y",
+//     info: function () {
+//       return `${this.title} by ${this.author},${this.pages} pages,
+//       ${this.read === "Y" ? `I read it before` : `I dont read yet`}`;
+//     },
+//   },
+//   {
+//     title: "Book 2",
+//     author: "Authour 2",
+//     pages: 512,
+//     read: "N",
+//     info: function () {
+//       return `${this.title} by ${this.author},${this.pages} pages,
+//         ${this.read === "Y" ? `I read it before` : `I dont read yet`}`;
+//     },
+//   },
+// ];
+
+// function showBooks() {
+//   for (let i = 0; i < myLibrary.length; i++) {
+//     let li = document.createElement("li");
+//     item = document.createTextNode(myLibrary[i].info());
+//     li.appendChild(item);
+//     ul.appendChild(li);
+//   }
+// }
+
+// showBooks();
+
+// // Add a click event listener to the "Open Dialog Box" button
+// openDialogBox.addEventListener("click", function () {
+//   let userDialog = document.getElementById("userDialog");
+//   let outputBox = document.querySelector(".output");
+//   let confirmBtn = document.getElementById("confirmBtn");
+//   let addToLibraryBtn = document.createElement("button");
+//   let addToLibrary = document.createTextNode("Add to library");
+
+//   // Define the modalOpen function
+//   function modalOpen() {
+//     userDialog.showModal();
+//   }
+
+//   // Add a click event listener to the "Confirm" button
+//   confirmBtn.addEventListener("click", function displayValues() {
+//     let bookTitleValue = document.getElementById("bookTitle").value;
+//     let authorValue = document.getElementById("author").value;
+//     let pagesValue = document.getElementById("pages").value;
+
+//     outputBox.textContent = `Book Title: ${bookTitleValue}, Author: ${authorValue}, Pages: ${pagesValue}   `;
+
+//     closeDialog();
+//     addToLibraryBtn.appendChild(addToLibrary);
+//     outputBox.appendChild(addToLibraryBtn);
+//   });
+
+//   addToLibraryBtn.addEventListener("click", addBook);
+
+//   function addBook() {
+//     console.log(myLibrary);
+//   }
+
+//   //  close the dialog
+//   function closeDialog() {
+//     userDialog.close();
+//   }
+
+//   // Show the modal dialog
+//   modalOpen();
+// });
