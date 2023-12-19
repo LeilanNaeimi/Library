fb272c70a08804be629a29a36f258b3e3476d89b
LeilanNaeimi <leilan.n@gmail.com>
2023-12-19 14:46:05 +0300

    add some logic

--- script.js
+++ script.js
@@ -1,78 +1,96 @@
-const myLibrary = [
-  {
-    title: "Book 1",
-    author: "Authour 1",
-    pages: 100,
-    read: "Y",
-    info: function () {
-      return `${this.title} by ${this.author},${this.pages} pages,
-      ${
-        this.read === "Y" ? `I read it before` : `Dont read yet`
-      } and serial no is: 10000}`;
-    },
-  },
-  {
-    title: "Book 2",
-    author: "Authour 2",
-    pages: 512,
-    read: "N",
-    info: function () {
-      return `${this.title} by ${this.author},${this.pages} pages,
-        ${
-          this.read === "Y" ? `I read it before` : `Dont read yet`
-        } and serial no is: 10000}`;
-    },
-  },
-];
+let openDialogBox = document.getElementById("openDialogBox");
+
+let myLibrary = [];
+// const myLibrary = [
+//   {
+//     title: "Book 1",
+//     author: "Authour 1",
+//     pages: 100,
+//     read: "Y",
+//     info: function () {
+//       return `${this.title} by ${this.author},${this.pages} pages,
+//       ${
+//         this.read === "Y" ? `I read it before` : `Dont read yet`
+//       } and serial no is: 10000}`;
+//     },
+//   },
+//   {
+//     title: "Book 2",
+//     author: "Authour 2",
+//     pages: 512,
+//     read: "N",
+//     info: function () {
+//       return `${this.title} by ${this.author},${this.pages} pages,
+//         ${
+//           this.read === "Y" ? `I read it before` : `Dont read yet`
+//         } and serial no is: 10000}`;
+//     },
+//   },
+// ];
 
 let card = document.querySelector(".card");
 let ul = document.querySelector("ul");
 let item = "";
-function Book() {
-  // the constructor...
-  //
+
+function showBooks(myLibrary) {
+  console.log(myLibrary.length);
   for (let i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i].title);
     let li = document.createElement("li");
     item = document.createTextNode(myLibrary[i].title);
-    console.log(li);
+    // `Title: ${myLibrary[i].title},Author  ${myLibrary[i].author}, Pages ${myLibrary[i].pages}.`
     li.appendChild(item);
     ul.appendChild(li);
   }
 }
 
-function addBookToLibrary() {
-  // do stuff here
-}
-
-Book();
+// showBooks(myLibrary);
 
 /******** add book btn */
-const showButton = document.getElementById("showDialog");
-const favDialog = document.getElementById("favDialog");
-const outputBox = document.querySelector("output");
-const selectEl = favDialog.querySelector("select");
-const confirmBtn = favDialog.querySelector("#confirmBtn");
 
-// "Show the dialog" button opens the <dialog> modally
-showButton.addEventListener("click", () => {
-  favDialog.showModal();
-});
+// Add a click event listener to the "Open Dialog Box" button
+openDialogBox.addEventListener("click", function () {
+  let userDialog = document.getElementById("userDialog");
+  let outputBox = document.querySelector(".output");
+  let confirmBtn = document.getElementById("confirmBtn");
+  let addToLibraryBtn = document.createElement("button");
+  let addToLibrary = document.createTextNode("Add to library");
 
-// "Favorite animal" input sets the value of the submit button
-selectEl.addEventListener("change", (e) => {
-  confirmBtn.value = selectEl.value;
-});
+  // Define the modalOpen function
+  function modalOpen() {
+    userDialog.showModal();
+  }
 
-// "Cancel" button closes the dialog without submitting because of [formmethod="dialog"], triggering a close event.
-favDialog.addEventListener("close", (e) => {
-  outputBox.value =
-    favDialog.returnValue === "default"
-      ? "No return value."
-      : `ReturnValue: ${favDialog.returnValue}.`; // Have to check for "default" rather than empty string
-});
+  // Add a click event listener to the "Confirm" button
+  confirmBtn.addEventListener("click", function displayValues() {
+    let bookTitleValue = document.getElementById("bookTitle").value;
+    let authorValue = document.getElementById("author").value;
+    let pagesValue = document.getElementById("pages").value;
 
-// Prevent the "confirm" button from the default behavior of submitting the form, and close the dialog with the `close()` method, which triggers the "close" event.
-confirmBtn.addEventListener("click", (event) => {
-  event.preventDefault(); // We don't want to submit this fake form
-  favDialog.close(selectEl.value); // Have to send the select box value here.
+    outputBox.textContent = `Book Title: ${bookTitleValue}, Author: ${authorValue}, Pages: ${pagesValue}   `;
+
+    myLibrary.title = bookTitleValue;
+    myLibrary.author = authorValue;
+    myLibrary.pages = pagesValue;
+
+    // Close the dialog after displaying the values
+    closeDialog();
+    addToLibraryBtn.appendChild(addToLibrary);
+    outputBox.appendChild(addToLibraryBtn);
+  });
+
+  addToLibraryBtn.addEventListener("click", addBookToLibrary);
+
+  function addBookToLibrary() {
+    showBooks(myLibrary);
+    console.log("add to library");
+  }
+
+  //  close the dialog
+  function closeDialog() {
+    userDialog.close();
+  }
+
+  // Show the modal dialog
+  modalOpen();
 });
